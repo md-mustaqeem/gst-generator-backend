5fc27546c9db75a99a2b24e20a5e8cd81a40d541
Md <mdmustaqeem820@gmail.com>
2026-06-24 13:18:09 +0530

    Fix CORS

--- src/main/java/com/gst/config/CorsConfig.java
+++ src/main/java/com/gst/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://YOUR-VERCEL-APP.vercel.app"
+                "https://gst-generator-frontend-qvp3ggyuh-md-sm-projects.vercel.app"
         ));
 
         config.setAllowedHeaders(List.of("*"));
